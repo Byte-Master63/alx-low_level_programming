a0171fb76a3870a39fe8cd3220e0877e5cb4590e
betty <betty@example.com>
2023-10-10 16:38:14 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	int count;
 
 	for (count = 0; count < 10; count++)
-}
+{
 	letter = 'a';
 
-	while (latter <= 'z')
+	while (letter <= 'z')
 {
 	_putchar(letter);
 	letter++
