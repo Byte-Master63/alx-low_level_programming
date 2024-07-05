13986b384ee7a904bd8f4291350d5373e24d75f0
Byte-Master63 <thokozanetek@gmail.com>
2024-07-05 03:57:48 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *s2);
+char _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
+char print_number(int n);
+char *infinit_add(char *b, int size);
+
+#endif
